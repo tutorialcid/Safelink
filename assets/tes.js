1042b7b7cfddbca7df93143e66195fe5fc10d789
tutorialcid <adaniblog852@gmail.com>
2017-04-05 01:29:40 -0700

    Create tes.js

---
+++ assets/tes.js
@@ -0,0 +1,5 @@
+<script src='http://localhost/safelink.js' type='text/javascript'/>
+<script type='text/javascript'>
+protected_links = &quot;marwanto606.xyz,mpti.ga,blogspot.com,blogger.com,linkedin.com,facebook.com,twitter.com,google.com,feedburner.com,mobile-10.com,slask.pl&quot;; 
+auto_safelink();
+</script>
