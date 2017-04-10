965648b9d3138cf02bb44e39be5d1f795df0b22a
tutorialcid <adaniblog852@gmail.com>
2017-04-09 19:35:28 -0700

    Update salink-tes.js

--- salink-tes.js
+++ salink-tes.js
@@ -1,5 +1,34 @@
 <script type='text/javascript'>
 //<![CDATA[
- var _0xdc80=["\x2F\x2F\x63\x64\x6E\x2E\x6A\x73\x64\x65\x6C\x69\x76\x72\x2E\x6E\x65\x74\x2F\x7A\x65\x72\x6F\x63\x6C\x69\x70\x62\x6F\x61\x72\x64\x2F\x32\x2E\x32\x2E\x30\x2F\x5A\x65\x72\x6F\x43\x6C\x69\x70\x62\x6F\x61\x72\x64\x2E\x73\x77\x66","\x63\x6F\x6E\x66\x69\x67","\x62\x74\x6E\x43\x6F\x70\x79","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x72\x65\x61\x64\x79","\x64\x69\x73\x70\x6C\x61\x79","\x73\x74\x79\x6C\x65","\x69\x6E\x6C\x69\x6E\x65\x2D\x62\x6C\x6F\x63\x6B","\x63\x6F\x70\x79","\x74\x65\x78\x74\x2F\x70\x6C\x61\x69\x6E","\x76\x61\x6C\x75\x65","\x74\x61\x72\x67\x65\x74","\x73\x65\x74\x44\x61\x74\x61","\x63\x6C\x69\x70\x62\x6F\x61\x72\x64\x44\x61\x74\x61","\x6F\x6E","\x61\x66\x74\x65\x72\x63\x6F\x70\x79","\x62\x74\x6E\x49\x6E\x66\x6F","\x62\x6C\x6F\x63\x6B","\x69\x6E\x70\x75\x74\x55\x52\x4C","","\x72\x65\x73\x75\x6C\x74\x4C\x69\x6E\x6B","\x65\x72\x72\x6F\x72","\x64\x65\x73\x74\x72\x6F\x79","\x6F\x6E\x6B\x65\x79\x75\x70","\x68\x74\x74\x70\x3A\x2F\x2F\x61\x64\x61\x6E\x69\x73\x6F\x65\x2E\x62\x6C\x6F\x67\x73\x70\x6F\x74\x2E\x63\x6F\x2E\x69\x64\x2F\x70\x2F\x73\x61\x66\x65\x6C\x69\x6E\x6B\x2E\x68\x74\x6D\x6C\x3F\x75\x72\x6C\x3D","\x65\x6E\x63\x6F\x64\x65"];ZeroClipboard[_0xdc80[1]]({swfPath:_0xdc80[0]});var client= new ZeroClipboard(document[_0xdc80[3]](_0xdc80[2]));client[_0xdc80[14]](_0xdc80[4],function(_0xe955x2){document[_0xdc80[3]](_0xdc80[2])[_0xdc80[6]][_0xdc80[5]]= _0xdc80[7];client[_0xdc80[14]](_0xdc80[8],function(_0xe955x2){_0xe955x2[_0xdc80[13]][_0xdc80[12]](_0xdc80[9],_0xe955x2[_0xdc80[11]][_0xdc80[10]])});client[_0xdc80[14]](_0xdc80[15],function(_0xe955x2){document[_0xdc80[3]](_0xdc80[16])[_0xdc80[6]][_0xdc80[5]]= _0xdc80[17];document[_0xdc80[3]](_0xdc80[18])[_0xdc80[10]]= _0xdc80[19];document[_0xdc80[3]](_0xdc80[20])[_0xdc80[10]]= _0xdc80[19]})});client[_0xdc80[14]](_0xdc80[21],function(_0xe955x2){ZeroClipboard[_0xdc80[22]]()});var output=document[_0xdc80[3]](_0xdc80[20]),input=document[_0xdc80[3]](_0xdc80[18]);input[_0xdc80[23]]= function(){if(input[_0xdc80[10]]!= _0xdc80[19]){output[_0xdc80[10]]= _0xdc80[24]+ Base64[_0xdc80[25]](this[_0xdc80[10]])}else {output[_0xdc80[10]]= _0xdc80[19]}}
+  ZeroClipboard.config({
+    swfPath: "//cdn.jsdelivr.net/zeroclipboard/2.2.0/ZeroClipboard.swf"
+  });
+  var client = new ZeroClipboard(document.getElementById('btnCopy'));
+  client.on('ready', function(event) {
+    document.getElementById('btnCopy').style.display = 'inline-block';
+
+    client.on('copy', function(event) {
+      event.clipboardData.setData('text/plain', event.target.value);
+    });
+
+    client.on('aftercopy', function(event) {
+      document.getElementById('btnInfo').style.display = 'block';
+      document.getElementById('inputURL').value = '';
+      document.getElementById('resultLink').value = '';
+    });
+  });
+
+  client.on('error', function(event) {
+    ZeroClipboard.destroy();
+  });
+  var output = document.getElementById('resultLink'),
+    input = document.getElementById('inputURL');
+  input.onkeyup = function() {
+    if (input.value != "") {
+      output.value = "http://adanisoe.blogspot.co.id/p/safelink.html?url=" + Base64.encode(this.value); //change with your link
+    } else {
+      output.value = "";
+    }
+  }
 //]]>
 </script>
