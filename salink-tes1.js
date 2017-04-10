d20ac2958b45f4bd519d3d0cf499b640211a4809
tutorialcid <adaniblog852@gmail.com>
2017-04-09 19:40:15 -0700

    Add files via upload

---
+++ salink-tes1.js
@@ -0,0 +1,30 @@
+ZeroClipboard.config({
+    swfPath: "//cdn.jsdelivr.net/zeroclipboard/2.2.0/ZeroClipboard.swf"
+  });
+  var client = new ZeroClipboard(document.getElementById('btnCopy'));
+  client.on('ready', function(event) {
+    document.getElementById('btnCopy').style.display = 'inline-block';
+
+    client.on('copy', function(event) {
+      event.clipboardData.setData('text/plain', event.target.value);
+    });
+
+    client.on('aftercopy', function(event) {
+      document.getElementById('btnInfo').style.display = 'block';
+      document.getElementById('inputURL').value = '';
+      document.getElementById('resultLink').value = '';
+    });
+  });
+
+  client.on('error', function(event) {
+    ZeroClipboard.destroy();
+  });
+  var output = document.getElementById('resultLink'),
+    input = document.getElementById('inputURL');
+  input.onkeyup = function() {
+    if (input.value != "") {
+      output.value = "http://adanisoe.blogspot.co.id/p/safelink.html?url=" + Base64.encode(this.value); //change with your link
+    } else {
+      output.value = "";
+    }
+  }
